6cddbefd400d0ab738747c6ec1fb149d6b38c7d8
kiskovi97 <kiskovi97@gmail.com>
2019-09-21 10:38:16 +0200

    FreeMoving

--- WaterBending/Assets/Scripts/GPUBased/MarchingCubes.cs
+++ WaterBending/Assets/Scripts/GPUBased/MarchingCubes.cs
@@ -4,9 +4,220 @@ using UnityEngine;
 
 namespace Assets.Scripts.GPUBased
 {
+    public class CubeCollection
+    {
+        public MarchingCubeShader[] Cubes { get; internal set; }
+        public float size = 20;
+
+        static readonly Vector3[] offsets = new Vector3[]
+        {
+            new Vector3(), Vector3.left , Vector3.left *2,
+            Vector3.left + Vector3.down, Vector3.left + Vector3.up, Vector3.left + Vector3.forward, Vector3.left + Vector3.back,
+            Vector3.right , Vector3.right *2,
+            Vector3.right + Vector3.down,Vector3.right + Vector3.up,Vector3.right + Vector3.forward,Vector3.right + Vector3.back,
+
+            Vector3.up , Vector3.up *2,
+            Vector3.up + Vector3.forward,Vector3.up + Vector3.back,
+            Vector3.down , Vector3.down *2,
+            Vector3.down + Vector3.forward,Vector3.down + Vector3.back,
+
+            Vector3.forward , Vector3.forward *2,
+            Vector3.back , Vector3.back *2,
+        };
+
+        static int C = 0;
+        static int L = 1;
+        static int LL = 2;
+        static int LD = 3;
+        static int DL = 3;
+        static int LU = 4;
+        static int UL = 4;
+        static int LF = 5;
+        static int FL = 5;
+        static int LB = 6;
+        static int BL = 6;
+
+        static int R = 7;
+        static int RR = 8;
+        static int RD = 9;
+        static int DR = 9;
+        static int RU = 10;
+        static int UR = 10;
+        static int RF = 11;
+        static int FR = 11;
+        static int RB = 12;
+        static int BR = 12;
+
+        static int U = 13;
+        static int UU = 14;
+        static int UF = 15;
+        static int FU = 15;
+        static int UB = 16;
+        static int BU = 16;
+
+        static int D = 17;
+        static int DD = 18;
+        static int DF = 19;
+        static int FD = 19;
+        static int DB = 20;
+        static int BD = 20;
+
+        static int F = 21;
+        static int FF = 22;
+        static int B = 23;
+        static int BB = 24;
+
+        public void Init(GameObject template, Vector3 position)
+        {
+            Cubes = new MarchingCubeShader[offsets.Length];
+            for (int i = 0; i < offsets.Length; i++)
+            {
+                var obj = Object.Instantiate(template, position + offsets[i] * size, new Quaternion());
+                Cubes[i] = obj.GetComponent<MarchingCubeShader>();
+            }
+
+        }
+
+        public void MoveLeft()
+        {
+            var offset = Vector3.right;
+            ShiftArray(new int[] { LL, L, C, R, RR }, offset);
+            ShiftArray(new int[] { LD, D, RD }, offset);
+            ShiftArray(new int[] { LU, U, RU }, offset);
+            ShiftArray(new int[] { LF, F, RF }, offset);
+            ShiftArray(new int[] { LB, B, RB }, offset);
+
+            ShiftArray(new int[] { FF }, offset);
+            ShiftArray(new int[] { BB }, offset);
+            ShiftArray(new int[] { UU }, offset);
+            ShiftArray(new int[] { DD }, offset);
+
+            ShiftArray(new int[] { UF }, offset);
+            ShiftArray(new int[] { UB }, offset);
+            ShiftArray(new int[] { DF }, offset);
+            ShiftArray(new int[] { DB }, offset);
+        }
+
+        public void MoveRight()
+        {
+            var offset = Vector3.left;
+            ShiftArray(new int[] { RR, R, C, L, LL }, offset);
+            ShiftArray(new int[] { RD, D, LD }, offset);
+            ShiftArray(new int[] { RU, U, LU }, offset);
+            ShiftArray(new int[] { RF, F, LF }, offset);
+            ShiftArray(new int[] { RB, B, LB }, offset);
+
+            ShiftArray(new int[] { FF }, offset);
+            ShiftArray(new int[] { BB }, offset);
+            ShiftArray(new int[] { UU }, offset);
+            ShiftArray(new int[] { DD }, offset);
+
+            ShiftArray(new int[] { UF }, offset);
+            ShiftArray(new int[] { UB }, offset);
+            ShiftArray(new int[] { DF }, offset);
+            ShiftArray(new int[] { DB }, offset);
+        }
+
+        public void MoveUp()
+        {
+            var offset = Vector3.up;
+            ShiftArray(new int[] { DD, D, C, U, UU }, offset);
+            ShiftArray(new int[] { DL, L, UL }, offset);
+            ShiftArray(new int[] { DR, R, UR }, offset);
+            ShiftArray(new int[] { DF, F, UF }, offset);
+            ShiftArray(new int[] { DB, B, UB }, offset);
+
+            ShiftArray(new int[] { FF }, offset);
+            ShiftArray(new int[] { BB }, offset);
+            ShiftArray(new int[] { RR }, offset);
+            ShiftArray(new int[] { LL }, offset);
+
+            ShiftArray(new int[] { RF }, offset);
+            ShiftArray(new int[] { RB }, offset);
+            ShiftArray(new int[] { LF }, offset);
+            ShiftArray(new int[] { LB }, offset);
+        }
+
+        public void MoveDown()
+        {
+            var offset = Vector3.down;
+            ShiftArray(new int[] { UU, U, C, D, DD }, offset);
+            ShiftArray(new int[] { UL, L, DL }, offset);
+            ShiftArray(new int[] { UR, R, DR }, offset);
+            ShiftArray(new int[] { UF, F, DF }, offset);
+            ShiftArray(new int[] { UB, B, DB }, offset);
+
+            ShiftArray(new int[] { FF }, offset);
+            ShiftArray(new int[] { BB }, offset);
+            ShiftArray(new int[] { RR }, offset);
+            ShiftArray(new int[] { LL }, offset);
+
+            ShiftArray(new int[] { RF }, offset);
+            ShiftArray(new int[] { RB }, offset);
+            ShiftArray(new int[] { LF }, offset);
+            ShiftArray(new int[] { LB }, offset);
+        }
+
+        public void MoveFront()
+        {
+            var offset = Vector3.back;
+            ShiftArray(new int[] { FF, F, C, B, BB }, offset);
+            ShiftArray(new int[] { FL, L, BL }, offset);
+            ShiftArray(new int[] { FR, R, BR }, offset);
+            ShiftArray(new int[] { FU, U, BU }, offset);
+            ShiftArray(new int[] { FD, D, BD }, offset);
+
+            ShiftArray(new int[] { UU }, offset);
+            ShiftArray(new int[] { DD }, offset);
+            ShiftArray(new int[] { RR }, offset);
+            ShiftArray(new int[] { LL }, offset);
+
+            ShiftArray(new int[] { RD }, offset);
+            ShiftArray(new int[] { RU }, offset);
+            ShiftArray(new int[] { LD }, offset);
+            ShiftArray(new int[] { LU }, offset);
+        }
+
+        public void MoveBack()
+        {
+            var offset = Vector3.forward;
+            ShiftArray(new int[] { BB, B, C, F, FF }, offset);
+            ShiftArray(new int[] { BL, L, FL }, offset);
+            ShiftArray(new int[] { BR, R, FR }, offset);
+            ShiftArray(new int[] { BU, U, FU }, offset);
+            ShiftArray(new int[] { BD, D, FD }, offset);
+
+            ShiftArray(new int[] { UU }, offset);
+            ShiftArray(new int[] { DD }, offset);
+            ShiftArray(new int[] { RR }, offset);
+            ShiftArray(new int[] { LL }, offset);
+
+            ShiftArray(new int[] { RD }, offset);
+            ShiftArray(new int[] { RU }, offset);
+            ShiftArray(new int[] { LD }, offset);
+            ShiftArray(new int[] { LU }, offset);
+        }
+
+        private void ShiftArray(int[] array, Vector3 offset)
+        {
+            var firstIndex = array[0];
+            var firstCube = Cubes[firstIndex];
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                var prevIndex = array[i];
+                var nextIndex = array[i + 1];
+                Cubes[prevIndex] = Cubes[nextIndex];
+            }
+            var lastIndex = array[array.Length - 1];
+            firstCube.transform.position += offset * size * array.Length;
+            Cubes[lastIndex] = firstCube;
+        }
+
+    }
+
     public class MarchingCubes : MonoBehaviour
     {
-        private MarchingCubeShader[] marchingCubes;
+        private CubeCollection cubeCollection;
         public GameObject marchingCubesObject;
 
         public new ParticleSystem particleSystem;
@@ -21,29 +232,42 @@ namespace Assets.Scripts.GPUBased
             size = MarchingCubeParameters.MatrixSize;
             matrix = new float[size * size * size];
 
-            var shaders = new List<MarchingCubeShader>();
-
             if (particles == null || particles.Length < particleSystem.main.maxParticles)
                 particles = new ParticleSystem.Particle[particleSystem.main.maxParticles];
 
-            for (int i=0; i<5; i++) 
-                for (int j=0; j<5; j++)
-                    for (int k = 0; k < 5; k++)
-                    {
-                    var obj = Instantiate(marchingCubesObject);
-                    var cube = obj.GetComponent<MarchingCubeShader>();
-                    shaders.Add(cube);
-                    var size = MarchingCubeParameters.MatrixSize;
-                    var correction = ((size - 1f) / size);
-                    obj.transform.position = new Vector3(i * obj.transform.lossyScale.x * correction, 
-                        j * obj.transform.lossyScale.y * correction, 
-                        k * obj.transform.lossyScale.z * correction);
-                }
-            marchingCubes = shaders.ToArray();
+            cubeCollection = new CubeCollection
+            {
+                size = marchingCubesObject.transform.lossyScale.x * (size - 1f) / (size)
+            };
+            cubeCollection.Init(marchingCubesObject, particleSystem.transform.position);
         }
 
         private void LateUpdate()
         {
+            if ((cubeCollection.Cubes[0].transform.position - particleSystem.transform.position).x < -marchingCubesObject.transform.lossyScale.x)
+            {
+                cubeCollection.MoveLeft();
+            }
+            if ((cubeCollection.Cubes[0].transform.position - particleSystem.transform.position).x > marchingCubesObject.transform.lossyScale.x)
+            {
+                cubeCollection.MoveRight();
+            }
+            if ((cubeCollection.Cubes[0].transform.position - particleSystem.transform.position).y < -marchingCubesObject.transform.lossyScale.y)
+            {
+                cubeCollection.MoveUp();
+            }
+            if ((cubeCollection.Cubes[0].transform.position - particleSystem.transform.position).y > marchingCubesObject.transform.lossyScale.y)
+            {
+                cubeCollection.MoveDown();
+            }
+            if ((cubeCollection.Cubes[0].transform.position - particleSystem.transform.position).z < -marchingCubesObject.transform.lossyScale.z)
+            {
+                cubeCollection.MoveBack();
+            }
+            if ((cubeCollection.Cubes[0].transform.position - particleSystem.transform.position).z > marchingCubesObject.transform.lossyScale.z)
+            {
+                cubeCollection.MoveFront();
+            }
             int numParticlesAlive = particleSystem.GetParticles(particles);
             var cubes = GetShader(particleSystem.transform.position);
             foreach (var cube in cubes)
@@ -60,7 +284,7 @@ namespace Assets.Scripts.GPUBased
         private MarchingCubeShader[] GetShader(Vector3 position)
         {
             var list = new List<MarchingCubeShader>();
-            foreach (MarchingCubeShader cube in marchingCubes)
+            foreach (MarchingCubeShader cube in cubeCollection.Cubes)
             {
                 var pos = cube.transform.position;
                 var scale = cube.transform.lossyScale;
@@ -73,7 +297,7 @@ namespace Assets.Scripts.GPUBased
             return list.ToArray();
         }
 
-        private float radius = 1f;
+        private float radius = 1.2f;
 
         public bool IsInCube(Vector3 scale, Vector3 pos)
         {
